504e321184c6f5ffbeea42dd01b471a019868693
lunicorn666 <ponyente42@gmail.com>
2020-11-18 13:36:26 +0100

    structure code and ignore headings and table of contents

--- data_extraction.py
+++ data_extraction.py
@@ -13,7 +13,11 @@ import bs4
 # verify if we get all the documents
 # hyphenation
 # some have English summary?
-
+# to keep or to discard headings / titles?
+# summary separate from full text?
+# remove date and place + SOU ID?
+# work with two-column format -> one case of suggested change of wording i.e. large part of the texts are really similar
+ 
 def retrieve_ids(sou_csv):
     """retrieves name of pdf and/or html document"""
     pass
@@ -53,19 +57,53 @@ def _freestanding_p(tag):
         if match:
             return match.span()[1] - match.span()[0] == len(tag.text)
         return False
-    #replace (tag.parent.name != "td") by not tag.a to keep two-column instances
+    #replace (tag.parent.name != "td") by not tag.a to keep two-column instances (e.g. H8B336)
     return tag.name == "p" and not tag.a and not _is_pagenum(tag)
 
 def _internal_link(tag):
     """filter out links that are not for navigation in the html"""
-    return tag.name == "a"and tag["href"].startswith("#page")
-    
+    return tag.name == "a" and tag["href"].startswith("#page")
+
+
+def _tables_outside_toc(tag):
+    """useful for finding headers and real tables"""
+    return tag.name == "table" and not tag.find("a")
+
+def _appendix(tag):
+    """find appendices in table of content"""
+    return tag.name == "td" and re.match("bilaga|bilagor", tag.text.casefold().strip())
+
+
+def insert_whitespace(children):
+    """correct for elements with multiple children with missing white space in between"""
+    text = ""
+    for i, child in enumerate(children[:-1]):
+        if (not (type(child) == bs4.element.NavigableString and child.endswith(" "))\
+            or (type(child) != bs4.element.NavigableString and child.text.endswith(" "))) and\
+            (not (type(children[i+1]) == bs4.element.NavigableString and children[i+1].startswith(" "))\
+             or (type(children[i+1]) != bs4.element.NavigableString and children[i+1].text.startswith(" "))):
+             if type(child) == bs4.element.NavigableString:
+                 text += f"{child} "
+             else:
+                 text += f"{child.text} "
+        else:
+            if type(child) == bs4.element.NavigableString:
+                 text += f"{child}"
+            else:
+                 text += f"{child.text}"
+    if type(children[-1]) == bs4.element.NavigableString:
+        text += f"{children[-1]} "
+    else:
+        text += f"{children[-1].text} "
+    return text
+
+
 def extract_from_html(text):
     """extract text as list of sentences and return full text and summary"""
     # text segments to distinguish
-    is_summary, is_english_summary, is_simple_summary = False, False, False
+    is_summary, is_english_summary, is_simple_summary, is_appendix = False, False, False, False
     summary_section_nb, end_of_summary = 0, ""
-    has_english_summary, has_simple_summary = False, False
+    has_english_summary, has_simple_summary, has_appendix = False, False, False
 
     # return variables
     summary, english_summary, simple_summary, full_text = [], [], [], []
@@ -77,16 +115,19 @@ def extract_from_html(text):
     soup = bs4.BeautifulSoup(requests.get(f'http:{text["content"]["dokument_url_html"]}').text)
     # there does not seem to be any difference between headings and text in the markup
     # hence, paragraphs can be a short title or multiple complete sentences
-    paragraphs = soup.find_all(_freestanding_p)
-    # to keep or to discard headings / titles?
-    # summary separate from full text?
-    # remove date and place + SOU ID?
-    # work with two-column format -> one case of suggested change of wording i.e. large part of the texts are really similar
-   
+    paragraphs = soup.find_all(_freestanding_p)  
     
     links = soup.find_all(_internal_link)
-
+    tables = soup.find_all(_tables_outside_toc)
+    table_types = set(tables)
+    copied_tables = "".join([str(t) for t in table_types if tables.count(t) > 1])
+    
+    # appendix = soup.find_all(_appendix)[0].text.strip()
+    # if appendix:
+    #     has_appendix = True
+    
     # find the name of the section following the summary
+    # and determine whether there are simplified or English summaries from the table of contents
     for i,hlink in enumerate(links):
         if not first_section and re.match(r"[A-Za-z]+", hlink.text) and hlink.parent.name == "td":
             first_section = hlink.text.strip(". ").casefold()
@@ -117,6 +158,8 @@ def extract_from_html(text):
             if summary_section_nb < 0:
                 #print(hlink.text, links[i].text, i)
                 end_of_summary = links[i].text.strip(". ")
+                if not first_section:
+                    first_section = end_of_summary
                 section_title = end_of_summary
                 print(f'1 "{end_of_summary}", "{summary_section_nb}"')
             if (summary_section_nb and links[i-1].text.strip(". ").startswith(str(summary_section_nb+1))):
@@ -124,6 +167,9 @@ def extract_from_html(text):
                 #print(summary_section_nb, links[i-1].text.strip(". "), summary_section_nb+1)
                 print(f'2 "{end_of_summary}" "{links[i-1]}" "{summary_section_nb}"')
                 section_title = end_of_summary
+                if not first_section:
+                    first_section = end_of_summary
+
             else:
                 section_title = links[i].text.strip(". ")
             if section_title.casefold() == "summary":
@@ -146,49 +192,37 @@ def extract_from_html(text):
             is_simple_summary = True
         return False
 
-    
+    # text extraction
     for i, p in enumerate(paragraphs):
-        #print(i, p, is_summary, is_table_of_c, is_english_summary, is_simple_summary, first_section)
+        #print(i, p, is_summary, is_table_of_c, is_english_summary, is_simple_summary, first_section, file=out)
         if p.text.startswith("SOU och Ds kan köpas från Norstedts Juridiks kundservice."):
             is_order_info = True
         elif is_order_info:
             if p.text.startswith("ISSN"):
                 is_order_info = False
-                continue      
+                continue
+        elif not is_table_of_c and p.text.casefold() == "innehåll":
+            is_table_of_c = True
+
         elif p.text.casefold() == "sammanfattning" and not summary:
             is_summary = True
             is_table_of_c = False
+
+        elif f"{p.text}</p></td>" in copied_tables:
+            continue
         elif is_summary and _is_end_of_summary(p):
             is_summary = False
-            print(f"stop summary '{p}'")
-        elif p.text.casefold() == "innehåll":
-            is_table_of_c = True
+
         if p.text.casefold().strip().endswith(first_section):
+
             is_table_of_c = False
+
         text = p.text
+        
         # correct for p-elements with multiple children, e.g. consecutive spans with missing white space
         children = list(p.children)
         if len(children) > 1:
-            text = ""
-            
-            for i, child in enumerate(children[:-1]):
-                    if (not (type(child) == bs4.element.NavigableString and child.endswith(" "))\
-                        or (type(child) != bs4.element.NavigableString and child.text.endswith(" "))) and\
-                        (not (type(children[i+1]) == bs4.element.NavigableString and children[i+1].startswith(" "))\
-                         or (type(children[i+1]) != bs4.element.NavigableString and children[i+1].text.startswith(" "))):
-                         if type(child) == bs4.element.NavigableString:
-                             text += f"{child} "
-                         else:
-                             text += f"{child.text} "
-                    else:
-                        if type(child) == bs4.element.NavigableString:
-                             text += f"{child}"
-                        else:
-                             text += f"{child.text}"
-            if type(children[-1]) == bs4.element.NavigableString:
-                text += f"{children[-1]} "
-            else:
-                text += f"{children[-1].text} "
+            text = insert_whitespace(children)
 
         if is_summary:
             if is_english_summary:
@@ -197,7 +231,7 @@ def extract_from_html(text):
                 simple_summary.append(text)
             else:
                 summary.append(text)
-        elif not is_table_of_c and not is_order_info: #?
+        elif not is_table_of_c and not is_order_info: 
             full_text.append(text)
         if not is_table_of_c and p.parent.name == "td":
             global also_tables
@@ -213,6 +247,8 @@ def print_to_files(id_, text, summary, english_summary, simple_summary):
             with open(f"documents/{name}_{id_}", "w") as ofile:
                 for line in content:
                     print(line, file=ofile)
+
+                    
 #docs = extract_from_json()
 # with open("documents.pickle","rb") as ifile:
 # docs = pickle.load(ifile)
